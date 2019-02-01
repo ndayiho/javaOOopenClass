52ad525017d191058bc36466875869f7f5fec717
Gasana.NHonore <ndayiho5@gmail.com>
2019-02-01 01:16:20 +0100

    automatisation parametrage front

--- src/jdbcF/AutomatisationKpk.java
+++ src/jdbcF/AutomatisationKpk.java
@@ -4,48 +4,87 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class AutomatisationKpk {
 
-    private static List<Integer> listMedias = new ArrayList<>();
+    private static String tableColumns = "";
+    private static String tableColumns_translation = "";
+
     public static void main(String[] args) {
         try {
-//            getMediaIds();
-            getResurt(listMedias);
+            getResurt();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
-    public static void getResurt(List<Integer> mediaIds)throws SQLException, ClassNotFoundException,IOException{
+    public static void getResurt() throws SQLException, ClassNotFoundException, IOException {
         Class.forName("com.mysql.jdbc.Driver");
-        String url = "jdbc:mysql://localhost:3306/kpk360_74?useSSL=false";
+        String url = "jdbc:mysql://localhost:3306/kpk360_5975?useSSL=false";
         String user = "root";
         String passwd = "root";
         Connection conn = DriverManager.getConnection(url, user, passwd);
-        StringBuilder builder = new StringBuilder();
-        for( int i = 0 ; i < mediaIds.size(); i++ ) {
-            builder.append(mediaIds.get(i)+",");
-        }
         String query = "select * from article";
 
         PreparedStatement state = conn.prepareStatement(query);
-//        System.out.println(state.toString());
         ResultSet result = state.executeQuery(query);
         ResultSetMetaData resultMeta = result.getMetaData();
-        PrintWriter file = new PrintWriter(new FileWriter("C:\\Users\\Gasana.NHonore\\Desktop\\Automatisation _front\\writejdbcTest.csv"));
-        for (int i = 1; i <= resultMeta.getColumnCount(); i++) {
-            file.print(resultMeta.getColumnName(i) + ";");
+        PrintWriter file = new PrintWriter(new FileWriter("C:\\Users\\Gasana.NHonore\\Desktop\\automatisation_front\\global.sql"));
+        tableColumns = resultMeta.getColumnName(1);
+        for (int i = 2; i <= resultMeta.getColumnCount(); i++) {
+            tableColumns = tableColumns.concat("," + resultMeta.getColumnName(i));
         }
-        file.print("\n");
         while (result.next()) {
-            file.println(result.getString("media_id") + ";" + result.getString("ean") + ";" + result.getString      ("Largeur") + ";" + result.getString("Hauteur"));
+            String test = result.getString(resultMeta.getColumnLabel(1));
+            for (int i = 2; i <= resultMeta.getColumnCount(); i++) {
+                if (result.getString(resultMeta.getColumnLabel(i)) != null) {
+//                    if column type is an integer
+                    if (resultMeta.getColumnType(i) == 4) {
+                        test = test.concat("," + result.getString(resultMeta.getColumnLabel(i)));
+                    } else {
+                        test = test.concat(",'" + result.getString(resultMeta.getColumnLabel(i)) + "'");
+                    }
+                } else {
+                    continue;
+                }
+            }
+            file.println("insert ignore into article(" + tableColumns + ")values(" + test + ");");
+        }
+
+        file.println("\n\n" + "-------- ****TRANSLATION*******----------" + "\n");
+
+        String query_translation = "select a.translation_id from article a left join translation tr  on(tr.translation_id = a.translation_id);";
+        PreparedStatement state2 = conn.prepareStatement(query_translation);
+        ResultSet result_translation = state2.executeQuery(query_translation);
+        ResultSetMetaData resultMeta_translation = result_translation.getMetaData();
+        PrintWriter file_translation = new PrintWriter(new FileWriter("C:\\Users\\Gasana.NHonore\\Desktop\\automatisation_front\\translationTest.sql"));
+        tableColumns_translation = resultMeta_translation.getColumnName(1);
+        for (int i = 2; i <= resultMeta_translation.getColumnCount(); i++) {
+            tableColumns_translation = tableColumns_translation.concat("," + resultMeta_translation.getColumnName(i));
+        }
+        String hello="test'";
+        while (result_translation.next()) {
+            String test2 = result_translation.getString(resultMeta_translation.getColumnLabel(1));
+            for (int i = 2; i <= resultMeta_translation.getColumnCount(); i++) {
+                if (result_translation.getString(resultMeta_translation.getColumnLabel(i)) != null) {
+//                    if column type is an integer
+                    if (resultMeta_translation.getColumnType(i) == 4) {
+                        test2 = test2.concat("," + result_translation.getString(resultMeta_translation.getColumnLabel(i)));
+                    } else {
+//                        result_translation.getString(resultMeta_translation.getColumnLabel(i).replace("'",hello));
+                        test2 = test2.concat(",'" + result_translation.getString(resultMeta_translation.getColumnLabel(i)) + "'");
+                    }
+
+                } else {
+                    continue;
+                }
+            }
+            file.println("insert ignore into translation(" + tableColumns_translation + ")values(" + test2 + ");");
         }
         file.close();
         result.close();
         state.close();
+        file_translation.close();
+        result_translation.close();
+        state2.close();
     }
-
 }
